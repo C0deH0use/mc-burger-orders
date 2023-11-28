8c598be6b4b568e4d5f38d848e6d028a404d2c90
Marek Malik <info@code-house.pl>
2023-11-28 13:49:30 +0100

    self review fixes

--- kitchen/topic_config.go
+++ kitchen/topic_config.go
@@ -3,6 +3,7 @@ package kitchen
 import (
 	"github.com/spf13/cast"
 	"mc-burger-orders/event"
+	"mc-burger-orders/log"
 	"os"
 	"strings"
 	"time"
@@ -12,6 +13,9 @@ func TopicConfigsFromEnv() *event.TopicConfigs {
 	kafkaAddressEnvVal := os.Getenv("KAFKA_ADDRESS")
 	kafkaAddress := strings.Split(kafkaAddressEnvVal, ",")
 	topic := os.Getenv("KAFKA_TOPICS__KITCHEN_REQUESTS_TOPIC_NAME")
+	if len(topic) <= 0 {
+		log.Error.Panicf("Kafka Topic `kitchen request events handler` name is missing")
+	}
 
 	numPartitions := 3
 	numPartitionsVal := os.Getenv("KAFKA_TOPICS__KITCHEN_REQUESTS_TOPIC_NUMBER_OF_PARTITIONS")
